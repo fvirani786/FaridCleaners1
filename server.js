499cf2523271a7e30da51e240608c7014ca65640
Farid Virani <fvirani786@gmail.com>
2024-06-13 13:19:25 -0500

    feat:changes

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const path = require('path');
 const { Service, Review, Store, User } = require('./models'); // Import models
 const logInfo = require('./middleware/logInfo'); // Import logInfo middleware
 
-dotenv.config();
+dotenv.config();ddfad 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
